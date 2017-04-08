9eac401b99ecef6337a93bcdf756462d42efb61c
rerost <gmailii@yahoo.co.jp>
2017-04-09 00:30:40 +0900

    init

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var babelify = require('babelify');
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require("vinyl-source-stream");
+ 
+gulp.task('browserify', function(){
+  var b = browserify({entries: ['./app/client/main.jsx']})
+    .transform(babelify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./app/build'));
+});
