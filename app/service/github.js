1c5af475ba10147299159c244f63e8e67402b66f
rerost <gmailii@yahoo.co.jp>
2017-04-30 02:18:51 +0900

    use octonodes method

--- app/service/github.js
+++ app/service/github.js
@@ -16,20 +16,9 @@ export default class Github {
 
   getNotificationList() {
     return new Promise((resolve, reject) => {
-      this.client.get('/notifications', {}, function (err, status, body, headers) {
-        if　(err) {
-          reject(err)
-        }
-        //console.log(body)
-        //user_id:         speakers service id
-        //content_id:      contents id (another service id need check)
-        //user_name:       service user name
-        //content:         Speech content
-        //reply_content:   reply about what content
-        //reply_user:      reply about what user_id
+      this.client.me().notifications({}, (a, body, b) => {
+        console.log(body)
         resolve(body)
-        //resolve(_items.map((item) => convert(item)))
-        //return callback(_items);
       })
     })
   }
