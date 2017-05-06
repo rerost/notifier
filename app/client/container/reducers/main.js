933739cf41f2716b21f0afeb00bc1b74ebca212a
rerost <gmailii@yahoo.co.jp>
2017-05-07 00:12:23 +0900

    use model for can update

--- app/client/container/reducers/main.js
+++ app/client/container/reducers/main.js
@@ -2,9 +2,10 @@ import { combineReducers } from 'redux';
 import { Map } from 'immutable';
 
 import * as columnActions from '../actions/column.js'
+import Column from '../models/column.js'
 
 const initialState = {
-  "https://api.github.com/notifications": Map({
+  "https://api.github.com/notifications": new Column({
     name: "Loading...",
     items: [],
     isFetching: false
@@ -15,35 +16,35 @@ const columnReducer = (state = initialState, action) => {
   switch (action.type) {
     case columnActions.REQUEST_ITEMS:
       return Object.assign({}, state, {
-        [action.url]: state[action.url].merge(Map({
+        [action.url]: state[action.url].update({
           name: "Loading...",
           items: [],
           isFetching: true,
-        }))
+        })
       });
     case columnActions.RECEIVE_ITEMS:
       return Object.assign({}, state, {
-        [action.url]: state[action.url].merge(Map({
+        [action.url]: state[action.url].update({
           items: action.items,
           isFetching: false
-        }))
+        })
       });
     case columnActions.RECEIVE_ITEM:
       return Object.assign({}, state, {
-        [action.url]: state[action.url].merge(Map({
+        [action.url]: state[action.url].update({
           items: [action.item, ...state[action.url].items],
           isFetching: false
-        }))
+        })
       })
     case columnActions.SET_NAME:
       return Object.assign({}, state, {
-        [action.url]: state[action.url].merge(Map({
+        [action.url]: state[action.url].update({
           name: action.name,
-        }))
+        })
       })
     case columnActions.ADD_COLUMN:
       return Object.assign({}, state, {
-        [action.url]: Map({
+        [action.url]: new Column({
           name: "Loading...",
           items: [],
           isFetching: false,
