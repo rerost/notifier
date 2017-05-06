681047c44950fe9a005c45548420b42cd92fb712
rerost <gmailii@yahoo.co.jp>
2017-05-07 01:24:13 +0900

    use immutable state

--- app/client/component/table.jsx
+++ app/client/component/table.jsx
@@ -8,12 +8,12 @@ const style = {
 
 export default class Table extends React.Component {
   render() {
-    const columns = Object.keys(this.props.columns).map((url) => {
+    const columns = this.props.columns.get("keys").map((url) => {
       return <Column
         key={url}
         url={url}
-        name={this.props.columns[url].get("name")}
-        items={this.props.columns[url].get("items")}
+        name={this.props.columns.get(url).get("name")}
+        items={this.props.columns.get(url).get("items")}
         addColumn={this.props.addColumn}
       />
     })
--- app/client/container/reducers/main.js
+++ app/client/container/reducers/main.js
@@ -4,52 +4,63 @@ import { Map } from 'immutable';
 import * as columnActions from '../actions/column.js'
 import Column from '../models/column.js'
 
-const initialState = {
+// State::[<URL, Column>]
+
+const initialState = Map({
+  "keys": ["https://api.github.com/notifications"],
   "https://api.github.com/notifications": new Column({
     name: "Loading...",
     items: [],
     isFetching: false
   })
-}
+})
 
 const columnReducer = (state = initialState, action) => {
   switch (action.type) {
     case columnActions.REQUEST_ITEMS:
-      return Object.assign({}, state, {
-        [action.url]: state[action.url].update({
+      return state.update(
+        action.url,
+        (value => value.update({
           name: "Loading...",
           items: [],
           isFetching: true,
-        })
-      });
+        }))
+      );
     case columnActions.RECEIVE_ITEMS:
-      return Object.assign({}, state, {
-        [action.url]: state[action.url].update({
+      return state.update(
+        action.url,
+        (value => value.update({
           items: action.items,
           isFetching: false
-        })
-      });
+        }))
+      )
     case columnActions.RECEIVE_ITEM:
-      return Object.assign({}, state, {
-        [action.url]: state[action.url].update({
-          items: [action.item, ...state[action.url].items],
+      return state.update(
+        action.url,
+        (value => value.update({
+          items: [action.item, ...state.get(action.url).items],
           isFetching: false
-        })
-      })
+        }))
+      )
     case columnActions.SET_NAME:
-      return Object.assign({}, state, {
-        [action.url]: state[action.url].update({
+      return state.update(
+        action.url,
+        (value => value.update({
           name: action.name,
-        })
-      })
+        }))
+      )
     case columnActions.ADD_COLUMN:
-      return Object.assign({}, state, {
-        [action.url]: new Column({
+      return state.update(
+        "keys",
+        (value => [...value, action.url])
+      ).set(
+        action.url,
+        new Column({
           name: "Loading...",
           items: [],
           isFetching: false,
         })
-      })
+      )
     default:
       return state
   }
