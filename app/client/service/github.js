c17bb34bd235a72ec4bc3fde7490590ec39b35d8
rerost <gmailii@yahoo.co.jp>
2017-05-07 17:24:27 +0900

    remove un used code

--- app/client/service/github.js
+++ app/client/service/github.js
@@ -14,55 +14,6 @@ export default class Github {
     this.client = github.client(token);
   }
 
-  getNotificationList() {
-    return new Promise((resolve, reject) => {
-      this.client.me().notifications({}, (a, body, b) => {
-        console.log(body)
-        resolve(body)
-      })
-    })
-  }
-
-  //[Promise]
-  getNotificationItems() {
-    return this.getNotificationList().then(
-      (body) => {
-        return body.map((notification) => this.convertToItem(notification))
-      },
-      () => {
-        return undefined
-      }
-    )
-  }
-
-  convertToItem(notification) {
-    return new Promise((resolve, reject) => {
-      this.client.get(notification.subject.latest_comment_url, {}, (err, status, body, headers) => {
-        if(err) {
-          reject(err)
-        }
-        const result = {
-          timestamp:     body.updated_at,
-          id:            body.id,
-          user_id:       body.user.id,
-          content_id:    notification.id,
-          user_name:     body.user.login,
-          content:       body.body,
-          reply_user:    2,
-          reply_content: 1,
-          url:           body.issue_url + "/comments",
-        }
-        resolve(result)
-      })
-    })
-  }
-
-  getNotification() {
-    return this.getNotificationItems().then(
-      (result) => result.map((promiseItem => promiseItem.then((i) => i, () => {})))
-    )
-  }
-
   getUrlType(url) {
     const truncted_url = url.replace(/^https:\/\/api.github.com\//, "")
     const regex_and_return = [
