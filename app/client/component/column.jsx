d8cab3edde0dc0a673a408f651c151418a50cb82
rerost <gmailii@yahoo.co.jp>
2017-05-12 01:15:54 +0900

    use material ui for title bar

--- app/client/component/column.jsx
+++ app/client/component/column.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ColumnItem from './column_item.jsx'
-import Github from '../service/github.js'
 import CSSModules from 'react-css-modules';
+
+import Github from '../service/github.js'
+
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+//See https://github.com/callemall/material-ui/issues/4670#issuecomment-231603600
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import ColumnItem from './column_item.jsx'
+import ColumnSettingButton from './column_setting_button.jsx'
 
 import styles from './column.scss'
 
@@ -19,20 +28,17 @@ class Column extends React.Component {
   render() {
     return(
       <div styleName="base">
-        <div styleName="head">
-          <div styleName="name">
-            {this.props.name}
-          </div>
-          {(() => {
-            if(!this.props.isMainColumn) {
-              return (
-                <div styleName="close-button" onClick={() => this.props.isMainColumn ? {} :this.props.deleteColumn(this.props.url)}>
-                  x
-                </div>
-              )
+        <MuiThemeProvider>
+          <AppBar
+            title={this.props.name}
+            iconElementRight={this.props.isMainColumn ? null :
+              <ColumnSettingButton
+                muiName='IconMenu'
+                deleteColumn={() => this.props.deleteColumn(this.props.url)}
+              />
             }
-          })()}
-        </div>
+          />
+        </MuiThemeProvider>
         <div styleName="body">
           {this.props.items.map((item) => <ColumnItem key={item.key} item={item} addColumn={this.props.addColumn}/>) }
         </div>
