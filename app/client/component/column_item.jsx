2cf7993f9d3c33a656e3633800eb87baa405cf6d
rerost <gmailii@yahoo.co.jp>
2017-05-12 21:25:54 +0900

    use material-ui card

--- app/client/component/column_item.jsx
+++ app/client/component/column_item.jsx
@@ -4,20 +4,35 @@ import PropTypes from 'prop-types';
 
 import Avatar from 'material-ui/Avatar';
 import { ListItem } from 'material-ui/List';
-import Divider from 'material-ui/Divider';
-
-const style = {
-}
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class ColumnItem extends React.Component {
   render() {
     const avatar = <Avatar src={this.props.item.avatar_url} />
     return(
-      <ListItem
-        leftAvatar={avatar}
-        primaryText={this.props.item.content}
-        onClick={() => this.props.addColumn(this.props.item.url)}
-      />
+      <Card>
+        <CardHeader
+          title={"@" + this.props.item.user_login}
+          avatar={this.props.item.avatar_url}
+        />
+        <CardText>
+          {this.props.item.content}
+        </CardText>
+        <CardActions style={{display: "flex", alignItems: "center"}}>
+          <FlatButton label="Fav" />
+          <IconMenu
+            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+          >
+            <MenuItem primaryText="Mark as Read" />
+            <MenuItem primaryText="Open in Github.com"/>
+          </IconMenu>
+        </CardActions>
+      </Card>
     );
   }
 }
